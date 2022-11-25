9f4c98ed6fc901918ef9afe9c37c093fb8a135be
Siroshun09 <siro.shun8@gmail.com>
2022-11-25 10:40:08 +0900

    build: init gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    `java-library`
+    id("me.champeau.jmh") version "0.6.8"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Write additional dependencies here.
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks {
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(17)
+    }
+
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+}
